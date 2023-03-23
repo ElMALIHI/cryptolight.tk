92cfecea1e5e87b1a7409b1a0b42152e3e61ea06
ElMALIHI <118868470+ElMALIHI@users.noreply.github.com>
2023-03-23 03:56:56 +0000

    fix admin

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfing) {
 
     eleventyConfing.addPassthroughCopy('./src/style.css');
     eleventyConfing.addPassthroughCopy('./src/assets');
+    eleventyConfing.addPassthroughCopy('./src/admin');
     eleventyConfing.addFilter("postDate", (dateObj) => {
 
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
